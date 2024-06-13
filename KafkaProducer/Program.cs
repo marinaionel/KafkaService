5e337183ed788e19d4a3f7096df76934698e6a29
Marina Ionel <marinicaionel@gmail.com>
2024-06-13 13:53:15 +0200

    Initial commit

---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,33 @@
+﻿using Confluent.Kafka;
+
+var configProducer = new ProducerConfig
+{
+    BootstrapServers = "localhost:9092"
+};
+
+using var producer = new ProducerBuilder<Null, string>(configProducer).Build();
+var topic = "test-topic";
+
+Console.WriteLine("Enter messages to send to Kafka (type 'exit' to quit):");
+
+while (true)
+{
+    var userInput = Console.ReadLine();
+    if (userInput.ToLower() == "exit")
+        break;
+
+    var message = new Message<Null, string> { Value = userInput };
+
+    producer.Produce(topic, message, deliveryReport => {
+        if (deliveryReport.Error.IsError)
+        {
+            Console.WriteLine($"Error: {deliveryReport.Error.Reason}");
+        }
+        else
+        {
+            Console.WriteLine($"Message sent: {deliveryReport.Message.Value}");
+        }
+    });
+
+    producer.Flush(TimeSpan.FromSeconds(10));
+}
---
+++ KafkaService/Program.cs
@@ -0,0 +1,23 @@
+﻿using Confluent.Kafka;
+
+var configConsumer = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092",
+    GroupId = "test-group",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using var consumer = new ConsumerBuilder<Ignore, string>(configConsumer).Build();
+consumer.Subscribe("test-topic");
+try
+{
+    while (true)
+    {
+        var message1 = consumer.Consume();
+        Console.WriteLine($"Received message: {message1.Value}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
